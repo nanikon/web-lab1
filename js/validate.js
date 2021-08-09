0ea3fe3f97b676243fb0f9412de3ef977b064ee9
nanikon <nata.nik.18@mail.ru>
2021-08-09 15:52:18 +0300

    add validate R checkbox

--- js/validate.js
+++ js/validate.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
     $(":button").click(function() {
-        $("input[type='button']").removeClass("active");
+        $("input[type='button']").removeClass("active"); //TODO решить стоит ли делать возможность не активной ни одну кнопку
         $(this).addClass("active");
     })
 
-    $(":checkbox").
+    $(":checkbox").click(function() {
+        $(":checkbox").prop("checked", false);
+        $(this).prop("checked", true);
+    })
 })
